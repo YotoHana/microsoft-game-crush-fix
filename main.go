9cd3927d4bd71b492a56e1df03cb723f51d224a2
Leonid Lugovoj <yotohana.dev@gmail.com>
2024-10-16 18:15:14 +0500

    -Fixed a bug with non-clickable buttons while disabling the fix -Now you can not resize the application window

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"os"
 	"os/exec"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
@@ -14,21 +12,16 @@ func main() {
 	a := app.New()
 	w := a.NewWindow("MicroFix")
 	w.Resize(fyne.NewSize(300, 80))
-	btnOn := widget.NewButton("Enable fix", killtask)
-	btnOff := widget.NewButton("Disable fix", addtask)
+	btnOn := widget.NewButton("Enable fix", func ()  {
+		exec.Command("taskkill", "/f", "/im", "explorer.exe").Run()
+		w.Content().Refresh()
+	})
+	btnOff := widget.NewButton("Disable fix", func ()  {
+		exec.Command("explorer").Start()
+		w.Content().Refresh()
+	})
 	content := container.NewVBox(btnOn, btnOff)
 	w.SetContent(content)
+	w.SetFixedSize(true)
 	w.ShowAndRun()
-}
-
-func killtask() {
-	c := exec.Command("taskkill", "/f", "/im", "explorer.exe")
-	c.Stdout = os.Stdout
-	c.Run()
-}
-
-func addtask() {
-	c := exec.Command("explorer")
-	c.Stdout = os.Stdout
-	c.Run()
 }
